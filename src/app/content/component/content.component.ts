64660f572b61cbda1f730207d9e8cd267dd83aea
François Plet <francisco_canari@hotmail.fr>
2017-08-29 12:10:44 +0200

    rebase

---
+++ src/app/content/component/content.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nod-content',
+    templateUrl: './content.component.html'
+})
+export class ContentComponent {
+    title = 'mon composant Content';
+}
